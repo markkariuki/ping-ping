bf8025533edb83f604e4f47d5449c08879108a52
david mark <markkariuki17@gmail.com>
2017-11-01 09:22:20 +0300

    forgot to add my js commits

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,44 @@
-$(document).ready(function()){
-  alert("welcome to the site")
-  $("form#numberFist").submit(function(event) {
+//business logic
+var userNumber;
 
+var numberArr = [];
+
+var pingPong = function(userInput) {
+for (var index = 1; index <= userInput; index++){
+  if ((index % 3 === 0) && (index % 5 === 0)) {
+    numberArr.push("pingpong");
+  } else if (index % 5 === 0){
+    numberArr.push("pong");
+  } else if (index % 3 === 0) {
+    numberArr.push("ping");
+  } else {
+    numberArr.push(index);
   }
 }
+return numberArr;
+};
+
+
+    function test(val){
+      console.log(val);
+    }
+
+//user interface logic
+$(document).ready(function() {
+alert("Welcome to Ping-Pong!!")
+  $("form#pong-number").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("input#ping-pong").val());
+    var results = pingPong(userInput);
+
+    $("#results").text("");
+    $("#results").append("<ul>");
+    results.forEach(function(result) {
+      $("#results").append('<li>' + result + '</li>');
+    });
+    $("#results").append("</ul>");
+
+    $("#results").slideIn();
+  });
+
+});
